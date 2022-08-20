1168157bd647c9800cac9d65b93d0266be180455
Ashish Antil <ashishantil.antil07@gmail.com>
2022-08-20 17:15:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<html>
+  <head></head>
+  <body>
+    
 <?php
 
 // $url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -73,3 +77,4 @@
   )
 
 ?>
+  </body>
