988cff9d53330704f851b0b7ee9dc50865ef7a08
Ashish Antil <ashishantil.antil07@gmail.com>
2022-08-20 21:00:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,29 @@
   <script>
     const params = new URL(location.href).searchParams;
     const text = params.get('text')?params.get('text'):'Hello there!';
-    const lines = text?.split(' | ');
+    const lines = text.split(' | ');
     const timePerChar = Number(params.get('timePerChar'))?Number(params.get('timePerChar')):100;
     const rest = Number(params.get('rest'))?Number(params.get('rest')):1000;
-    const color = params.get('color');
-    let alignment = params.get('alignment');
-    const background = params.get('background')?params.get('background'):0;
-    const family = params.get('family');
-    const size = params.get('size');
+    const color = params.get('color')?params.get('color'):'gray';
+    let alignment = params.get('alignment')?params.get('alignment'):'left';
+    const background = params.get('background')?params.get('background'):'transparent';
+    const family = params.get('family')?params.get('family'):monospace;
+    const size = params.get('size')?params.get('size'):20;
     let j=0;
 
     if(text){
       const textElem = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-      alignment?.toLowerCase()==='center'?alignment='middle':0;
+      alignment.toLowerCase()==='center'?alignment='middle':0;
       document.querySelector('svg').append(textElem);
-      if(alignment){
-        textElem.setAttribute('dominant-baseline', alignment);
-        textElem.setAttribute('text-anchor', alignment);
-        alignment==='middle'?textElem.style.transform = 'translate(50%, 20px)':textElem.style.transform = 'translate(0px, 20px)';
-      }
-      color?textElem.style.fill = color:0;
-      size?textElem.style.fontSize = size:0;
-      family?textElem.style.fontFamily = family:0;
-      background?textElem.style.background = background:0;
+
+      textElem.setAttribute('dominant-baseline', alignment);
+      textElem.setAttribute('text-anchor', alignment);
+      alignment==='middle'?textElem.style.transform = 'translate(50%, 20px)':textElem.style.transform = 'translate(0px, 20px)';
+
+      textElem.style.fill = color:0;
+      textElem.style.fontSize = size:0;
+      textElem.style.fontFamily = family:0;
+      textElem.style.background = background:0;
 
       function lineTyper(line){
         line = lines[j].trim()
@@ -51,6 +51,7 @@
           }
         }, timePerChar)
       }
+      
       document.replaceChild(document.querySelector('svg'), document.documentElement);
       lineTyper(lines[j])
     }
